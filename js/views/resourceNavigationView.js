88d7694b2496e83d92973dc9b105a7febbffdd75
bjones <bjones@transcendcomputing.com>
2013-02-25 10:45:54 -0600

    added alert as globally defined

--- js/views/resourceNavigationView.js
+++ js/views/resourceNavigationView.js
@@ -4,7 +4,7 @@
  * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
  */
 /*jshint smarttabs:true */
-/*global define:true console:true requirejs:true require:true*/
+/*global define:true console:true requirejs:true require:true alert:true*/
 define([
         'jquery',
         'underscore',
