bc0e335fadbe42963e044fe9a93039b9a51be71b
Curtis Stewart <cstewart@momentumsi.com>
2013-03-29 11:46:01 -0500

    Enhanced users table in tenant details and added confirmRemove. Currently buttons make alerts and no backend calls.

---
+++ js/openstack/views/identity/openstackTenantConfirmRemoveView.js
@@ -0,0 +1,64 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+        'jquery',
+        'underscore',
+        'backbone',
+        'common',
+        'icanhaz',
+        'views/dialogView',
+        'text!templates/openstack/identity/openstackTenantConfirmRemoveTemplate.html',
+        'jquery.ui.selectmenu',
+        'jquery.multiselect',
+        'jquery.multiselect.filter'
+], function( $, _, Backbone, Common, ich, DialogView, tenantConfirmRemoveTemplate ) {
+
+    var TenantConfirmRemoveView = DialogView.extend({
+        template: tenantConfirmRemoveTemplate,
+
+        events: {
+            "dialogclose": "close",
+            "change input#enabled_input": "toggleEnabled"
+        },
+
+        initialize: function(options) {
+            this.message = options.message;
+            this.render();
+        },
+        
+        render: function() {
+            var createView = this;
+            ich.addTemplate("tenant_confirm_remove_template", this.template);
+            this.$el.html( ich.tenant_confirm_remove_template({message: this.message}) );
+            this.$el.dialog({
+                autoOpen: true,
+                title: "Confirm Remove Users",
+                width:500,
+                minHeight: 150,
+                resizable: false,
+                modal: true,
+                buttons: {
+                    Confirm: function () {
+                        createView.confirm();
+                    },
+                    Cancel: function() {
+                        createView.cancel();
+                    }
+                }
+            }); 
+        },
+
+        confirm: function() {
+            Common.vent.trigger("tenant:confirmRemove");
+            this.cancel();
+        }
+
+    });
+    
+    return TenantConfirmRemoveView;
+});
