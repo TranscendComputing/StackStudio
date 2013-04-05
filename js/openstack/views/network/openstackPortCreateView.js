f62b862ccd43733edf84f75835e0f3adc99aeb24
Curtis Stewart <cstewart@momentumsi.com>
2013-04-05 11:33:38 -0500

    Added openstackPort create support (untested because folsom issues).  Added comments for documentation around stickit for network and subnet createViews

--- js/openstack/views/network/openstackPortCreateView.js
+++ js/openstack/views/network/openstackPortCreateView.js
@@ -15,29 +15,63 @@ define([
         'text!templates/openstack/network/openstackPortCreateTemplate.html',
         '/js/openstack/models/network/openstackPort.js',
         '/js/openstack/collections/network/openstackPorts.js',
+        '/js/openstack/collections/network/openstackNetworks.js',
         'jquery.ui.selectmenu',
         'jquery.multiselect',
-        'jquery.multiselect.filter'
-], function( $, _, Backbone, Common, ich, DialogView, portCreateTemplate, Port, Ports ) {
+        'jquery.multiselect.filter',
+        'backbone.stickit'
+], function( $, _, Backbone, Common, ich, DialogView, portCreateTemplate, Port, Ports, Networks ) {
 
-    var VolumeCreateView = DialogView.extend({
-        
+    var PortCreateView = DialogView.extend({
         credentialId: undefined,
-
         region: undefined,
-        
         collection: new Ports(),
-
+        collectionsCount: 1, // collectionsCount tells stickit how many collections to wait for before initializing bindings
         template: portCreateTemplate,
 
         events: {
             "dialogclose": "close"
         },
 
+        /**
+         *    backbone.stickit bindings to map selectors to model attributes
+         *    NOTE: 'this' has scope of view in selectOptions
+         *    @type {Object}
+         */
+        bindings: {
+            '#admin_state_up_checkbox': 'admin_state_up',
+            '#allocation_pools_input': {
+                observe: 'allocation_pools'
+            },
+            '#routes_input': {
+                observe: 'routes'
+            },
+            '#name_input': 'name',
+            '#mac_address_input': 'mac_address',
+            'select#network_select': {
+                observe: 'network_id',
+                selectOptions: {
+                    collection: 'this.networks',
+                    labelPath: 'name',
+                    valuePath: 'id',
+                    defaultOption: {
+                        label: "Select Network",
+                        value: null
+                    },
+                    onGet: function(value, options) {
+                        return value;
+                    }
+                }
+            }
+        },
+
         initialize: function(options) {
             this.credentialId = options.cred_id;
             this.region = options.region;
             this.model = new Port({}, {collection: this.collection});
+            this.networks = new Networks();
+            this.networks.on("reset", this.applyBindings, this); // .applyBindings method defined in DialogView
+            this.networks.fetch({ data: $.param({ cred_id: this.credentialId, region: this.region}), reset: true });
             this.render();
         },
         
@@ -60,27 +94,18 @@ define([
                         createView.cancel();
                     }
                 }
-            }); 
-            $("select").selectmenu();
+            });
 
             // This line adds required asterisk to all fields with class 'required'
             this.$(".required").after("<span class='required'/>");
         },
 
         create: function() {
-            this.model.set({
-                name: $("#name_input").val()
-            });
-            if(!this.model.isValid())
-            {
-                Common.errorDialog("Validation Error", this.model.validationError);
-            }else{
-                this.model.create(this.credentialId, this.region); 
-                this.$el.dialog('close');
-            }
+            this.model.create(this.credentialId, this.region); 
+            this.$el.dialog('close');
         }
 
     });
     
-    return VolumeCreateView;
+    return PortCreateView;
 });
