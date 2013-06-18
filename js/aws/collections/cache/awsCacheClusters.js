29fae2e48ef2b786df52f470e7dc716df98eb45e
Dev Mohanty (MSI) <dmohanty@Devs-MacBook-Pro.local>
2013-06-18 11:05:00 -0500

    completed elasticache associated collections

---
+++ js/aws/collections/cache/awsCacheClusters.js
@@ -0,0 +1,27 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+        'jquery',
+        'backbone',
+        '/js/aws/models/cache/awsCacheCluster.js',
+        'common'
+], function( $, Backbone, CacheCluster, Common ) {
+    'use strict';
+
+
+    var CacheClusterList = Backbone.Collection.extend({
+
+        model: CacheCluster,
+
+        url: Common.apiUrl + '/stackstudio/v1/cloud_management/aws/cache/clusters'
+        
+    });
+
+    return CacheClusterList;
+
+});
