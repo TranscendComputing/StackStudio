6b62cf1e2d54db4d742b8142c471efdd05ccf7b2
Dev Mohanty (MSI) <dmohanty@Devs-MacBook-Pro.local>
2013-06-19 08:38:48 -0500

    completed ClustersAppView

--- js/aws/views/cache/awsClustersAppView.js
+++ js/aws/views/cache/awsClustersAppView.js
@@ -31,9 +31,9 @@ define([
         
         idColumnNumber: 0,
         
-        model: RelationalDatabase,
+        model: CacheCluster,
         
-        collectionType: RelationalDatabases,
+        collectionType: CacheClusters,
         
         type: "cache",
         
@@ -41,17 +41,42 @@ define([
         
         events: {
             'click .create_button': 'createNew',
+            'click #action_menu ul li': 'performAction',
             'click #resource_table tr': 'clickOne'
         },
 
         initialize: function() {
+            if(options.cred_id) {
+                this.credentialId = options.cred_id;
+            }
+            if(options.region) {
+                this.region = options.region;
+            }
             this.render();
+            
+            var cacheApp = this;
+            Common.vent.on("cacheAppRefresh", function() {
+                cacheApp.render();
+            });
+            
         },
-
+        
+        performAction: function(event) {
+            var cluster = this.collection.get(this.selectedId);
+            
+            switch(event.target.text)
+            {
+            case "Delete":
+                cluster.destroy(this.credentialId, this.region);
+                break;
+            }
+        },
+        /*
         render: function() {
             var featureNotImplemented = new FeatureNotImplementedView({feature_url: "https://github.com/TranscendComputing/StackStudio/issues/8", element: "#resource_app"});
             featureNotImplemented.render();
         },
+        */
         
         toggleActions: function(e) {
             //Disable any needed actions
