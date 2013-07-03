c5def856cd14a1cd2aed3cf22e277ad5aa7c9137
Barrett <barretthjones@gmail.com>
2013-07-03 13:31:06 -0500

    added support for openstack dns using topstack services

---
+++ js/topstack/models/dns/topstackHostedZone.js
@@ -0,0 +1,37 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+        'models/resource/resourceModel',
+        'common'
+], function( ResourceModel, Common ) {
+    'use strict';
+
+    var HostedZone = ResourceModel.extend({
+
+        defaults: {
+            id: '',
+            caller_reference: '',
+            change_info: '',
+            description: '',
+            domain: '',
+            nameservers: ''
+        },
+
+        create: function(options, credentialId) {
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/topstack/dns/hosted_zones?cred_id=" + credentialId;
+            this.sendAjaxAction(url, "POST", {"hosted_zone": options}, "hostedZoneAppRefresh");
+        },
+
+        destroy: function(credentialId) {
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/topstack/dns/hosted_zones/" + this.attributes.id + "?_method=DELETE&cred_id=" + credentialId;
+            this.sendAjaxAction(url, "POST", undefined, "hostedZoneAppRefresh");
+        }
+    });
+
+    return HostedZone;
+});
---
+++ js/topstack/models/dns/topstackRecordSet.js
@@ -0,0 +1,53 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+        'jquery',
+        'backbone',
+        'models/resource/resourceModel',
+        'common'
+], function( $, Backbone, ResourceModel, Common ) {
+    'use strict';
+
+    var RecordSet = ResourceModel.extend({
+
+        defaults: {
+            ResourceRecords: [],
+            Name: '',
+            Type: '',
+            TTL: ''
+        },
+
+        get: function(attr) {
+            if(typeof this[attr] === 'function') {
+                var attribute = this[attr]();
+                return attribute;
+            }
+            
+            return Backbone.Model.prototype.get.call(this, attr);
+        },
+        
+        ResourceRecords: function() {
+            var resourceRecords = "";
+            $.each(this.attributes.ResourceRecords, function(index, record) {
+                if(index === 0) {
+                    resourceRecords = record;
+                }else {
+                    resourceRecords = resourceRecords + "<br />" + record;
+                }
+            });
+            return resourceRecords;
+        },
+
+        change: function(options, credentialId, region) {
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/topstack/dns/hosted_zones/" + options["hosted_zone_id"] + "/record_sets/change?_method=PUT&cred_id=" + credentialId + "&region=" + region;
+            this.sendAjaxAction(url, "POST", {"record_set": options}, "recordSetRefresh");
+        }
+    });
+
+    return RecordSet;
+});
