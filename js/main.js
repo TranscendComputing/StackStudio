c5e0aa5857e40404214517994eb64f8399893343
Barrett <bjones@momentumsi.com>
2013-08-26 16:31:20 -0500

    removed msp dashboard due to not working with ip

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ if (DEBUG) {
 
 require(['./common'], function (common) {
     var dashboardView;
+    dashboardView = "views/dashboardView";
+    common.rssFeed = "http://www.transcendcomputing.com/feed/";
+    /* NOT WORKING
     var site = window.location.hostname.split(".")[0];
     var siteParam = window.location.search.replace("?site=", "");
     if(site.indexOf("localhost") > -1 || site.indexOf("stackstudio") > -1 || site.indexOf("devessex") > -1 || site.length < 2)
@@ -36,6 +39,7 @@ require(['./common'], function (common) {
         require([siteJs], function(){});
         dashboardView = "views/mspDashboardView";
     }
+    */
     require([
             'views/account/navLogin',
              dashboardView,
