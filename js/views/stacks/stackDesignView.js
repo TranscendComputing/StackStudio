c6472febf1139f5a877bba3299668013cdf960fb
Barrett <bjones@momentumsi.com>
2013-08-28 17:51:30 -0500

    added initial content to editor and resource tree

--- js/views/stacks/stackDesignView.js
+++ js/views/stacks/stackDesignView.js
@@ -11,8 +11,10 @@ define([
         'backbone',
         'common',
         'text!templates/stacks/stackDesignTemplate.html',
-        'ace'
-], function( $, _, Backbone, Common, stacksDesignTemplate, ace ) {
+        'ace-cdn',
+        'ace/mode/json',
+        'jquery.jstree'
+], function( $, _, Backbone, Common, stacksDesignTemplate) {
     'use strict';
 
     var StackDesignView = Backbone.View.extend({
@@ -21,29 +23,98 @@ define([
 
         editor: undefined,
 
+        newTemplateResources: undefined,
+
+        newResourceTree: undefined,
+
         events: {
-            
+            "click .jstree_custom_item": "treeFolderClick"
         },
 
         initialize: function() {
             $("#design_time_content").html(this.el);
             this.$el.html(this.template);
             this.editor = ace.edit("design_editor");
-            this.editor.setTheme("ace/theme/twilight");
-            this.editor.getSession().setMode("ace/mode/json");
-            
-
-            this.editor.on("change", this.handleChange);
+            this.editor.setTheme("ace/theme/monokai");
+            this.editor.getSession().setUseWorker(false);
+            this.editor.getSession().setMode(new (require("ace/mode/json")).Mode);
+            this.editor.getSession().setValue('{' +
+                    '\n\t"AWSTemplateFormatVersion": "2010-09-09",' +
+                    '\n\t"Description": "New template created in StackStudio.",' +
+                    '\n\t"Parameters": {},' +
+                    '\n\t"Mappings": {},' +
+                    '\n\t"Resources": {},' +
+                    '\n\t"Outputs": {}' +
+                '\n}'
+            );
         },
 
         render: function() {
-            
+            this.newResourceTree = $("#new_resources").jstree({ 
+                // List of active plugins
+                "plugins" : [ 
+                    "json_data", "crrm", "themeroller"
+                ],
+                
+                "core": {
+                    "animation": 0
+                 },
+
+                "json_data" : { 
+                    "ajax": {
+                        "url": "samples/cloud_resources.json",
+                        "success": function(data) {
+                            var services = {};
+                            var itemId;
+                            $.each(data, function(index, d) {
+                                 if (services[d.service] === undefined) {
+                                     services[d.service] = [];
+                                 }
+                                 //Add reference to parent tree
+                                 d.parent_tree = "#new_resources";
+                                 itemId = d.label.toLowerCase().replace(/\s/g, "_");
+                                 services[d.service].push({
+                                     "data": {
+                                         "title": d.label, 
+                                         "attr": {
+                                             "id": itemId, 
+                                             "class": "new_item_link"
+                                         } 
+                                     },
+                                     "attr": {"id": itemId + "_container"},
+                                     "metadata": d
+                                 });
+                            });
+                            
+                            var treeData = [];
+                            $.each(services, function(s, v) {
+                                treeData.push({
+                                    data: s,
+                                    children: v,
+                                    "metadata": {"parent_tree": "#new_resources"} 
+                                });
+                            });
+                            return treeData;
+                        }
+                    },
+                    "correct_state": false
+                },
+                
+                "themeroller": {
+                    "item": "jstree_custom_item"
+                }
+            });
         },
 
-        handleChange: function(e) {
-            var editor = ace.edit("design_editor");
-            var content = editor.getValue();
-            Common.vent.trigger('project:updateTemplate', content);
+        treeFolderClick: function(event) {
+            if($(event.target.parentElement).hasClass("jstree-closed")) {
+                $(event.target.parentElement).removeClass("jstree-closed");
+                $(event.target.parentElement).addClass("jstree-open");
+            }else{
+                $(event.target.parentElement).removeClass("jstree-open");
+                $(event.target.parentElement).addClass("jstree-closed");
+            }
+            return false;
         }
     });
 
