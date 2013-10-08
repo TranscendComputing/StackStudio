c74dc2167abbaa1d90be4219bad96583c858af4b
Dev Mohanty <dmohanty09@austincollege.edu>
2013-10-08 17:36:21 -0500

    Implemented rds sec group edit rules for aws

--- js/aws/models/rds/awsDBSecurityGroup.js
+++ js/aws/models/rds/awsDBSecurityGroup.js
@@ -29,6 +29,26 @@ define([
         destroy: function(credentialId, region) {
             var url = Common.apiUrl + "/stackstudio/v1/cloud_management/aws/rds/security_groups/" + this.attributes.id + "?_method=DELETE&cred_id=" + credentialId + "&region=" + region;
             this.sendAjaxAction(url, "POST", undefined, "securityGroupAppRefresh");
+        },
+        
+        addCIDR: function(cidr_val,credentialId, region){
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/aws/rds/security_groups/"+this.attributes.id+"/ipranges?cred_id=" + credentialId + "&region=" + region;
+            this.sendAjaxAction(url, "POST", {"cidrip": cidr_val}, "securityGroupAppRefresh","CIDR Block Authorized");
+        },
+        
+        addNovaGroup: function(group_val,credentialId, region){
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/aws/rds/security_groups/"+this.attributes.id+"/ec2_groups?cred_id=" + credentialId + "&region=" + region;
+            this.sendAjaxAction(url, "POST", {"ec2_group": group_val}, "securityGroupAppRefresh","Nova Group Authorized");
+        },
+        
+        revokeCIDR: function(cidr_val,credentialId, region){
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/aws/rds/security_groups/"+this.attributes.id+"/revoke_ipranges?cred_id=" + credentialId + "&region=" + region;
+            this.sendAjaxAction(url, "POST", {"cidrip": cidr_val}, "securityGroupAppRefresh","CIDR Block Revoked");
+        },
+        
+        revokeNovaGroup: function(group_val,credentialId, region){
+            var url = Common.apiUrl + "/stackstudio/v1/cloud_management/aws/rds/security_groups/"+this.attributes.id+"/revoke_ec2_groups?cred_id=" + credentialId + "&region=" + region;
+            this.sendAjaxAction(url, "POST", {"ec2_group": group_val}, "securityGroupAppRefresh","Nova Group Revoked");
         }
 
     });
