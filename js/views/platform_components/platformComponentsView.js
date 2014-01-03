a9a4a5675d6ca69f3ddee55e72871ca6ced4a66f
Barrett <barretthjones@gmail.com>
2014-01-02 19:38:07 -0600

    updated to build table from mock data insead of hardcode

--- js/views/platform_components/platformComponentsView.js
+++ js/views/platform_components/platformComponentsView.js
@@ -38,22 +38,22 @@ define([
             this.configManagers.fetch({reset:true});
             if(this.currentConfigManager) {
                 var configManagerName = this.currentConfigManager.attributes.name;
-                $("#chef_select_button_label").html("Selected Chef: " + configManagerName);
+                $("#select_button_label").html("Selected Chef: " + configManagerName);
                 $("#default_landing_view").hide();
                 if(this.currentConfigManager.attributes["continuous_integration_servers"].length > 0) {
                     $("#continuous_integration_setup_landing_view").hide();
-                    $("#chef_continuous_integration_view").show();
+                    $("#continuous_integration_table_view").show();
                     this.renderContinuousIntegration();
                 } else {
-                    $("#chef_continuous_integration_view").hide();
+                    $("#continuous_integration_table_view").hide();
                     $("#selected_cm_label").html(configManagerName);
                     $("#continuous_integration_setup_landing_view").show();
                 }
             } else {
-                $("#chef_continuous_integration_view").hide();
+                $("#continuous_integration_table_view").hide();
                 $("#continuous_integration_setup_landing_view").hide();
                 $("#default_landing_view").show();
-                $("#chef_select_button_label").html("Select Chef");
+                $("#select_button_label").html("Select Chef");
             }
         },
 
@@ -68,7 +68,100 @@ define([
         },
 
         renderContinuousIntegration: function() {
-            // Work for displaying the tests
+            if(this.currentConfigManager) {
+                // Set default values
+                var componentName = "Name";
+                var components = [];
+                switch(this.currentConfigManager.attributes.type) {
+                    case "chef":
+                        this.currentConfigManager.cookbooks = [
+                            {
+                                "name": "apigee",
+                                "community": false,
+                                "ci_presence": true,
+                                "status": {
+                                    "rspec_status": "NONE",
+                                    "foodcritic_status": "PASSING",
+                                    "syntax_status": "NONE",
+                                    "vagrant_ubuntu-12.04_status": "NONE",
+                                    "vagrant_centos-6.4_status": "NONE",
+                                    "sync_status": "VERSION_NOT_FOUND_IN_REPO"
+                                }
+                            },
+                            {
+                                "name": "att_postgresql",
+                                "community": false,
+                                "ci_presence": true,
+                                "status": {
+                                    "rspec_status": "PASSING",
+                                    "foodcritic_status": "PASSING",
+                                    "syntax_status": "PASSING",
+                                    "vagrant_ubuntu-12.04_status": "PASSING",
+                                    "vagrant_centos-6.4_status": "PASSING",
+                                    "sync_status": "IN_SYNC"
+                                }
+                            },
+                            {
+                                "name": "oauth_baseos",
+                                "community": false,
+                                "ci_presence": true,
+                                "status": {
+                                    "rspec_status": "NONE",
+                                    "foodcritic_status": "PASSING",
+                                    "syntax_status": "PASSING",
+                                    "vagrant_ubuntu-12.04_status": "FAILING",
+                                    "vagrant_centos-6.4_status": "FAILING",
+                                    "sync_status": "IN_SYNC"
+                                }
+                            },
+                            {
+                                "name": "postgresql",
+                                "community": true,
+                                "ci_presence": false,
+                                "status": {
+                                    "rspec_status": "NONE",
+                                    "foodcritic_status": "NONE",
+                                    "syntax_status": "NONE",
+                                    "vagrant_ubuntu-12.04_status": "NONE",
+                                    "vagrant_centos-6.4_status": "NONE",
+                                    "sync_status": "NOT_FOUND_IN_REPO"
+                                }
+                            },
+                        ];
+                        componentName = "Cookbook Name";
+                        components = this.currentConfigManager.cookbooks;
+                        break;
+                }
+
+                if(components.length > 0) {
+                    var thisView = this;
+                    $("#continuous_integration_table").empty();
+                    // Build Header of Table
+                    var headerColumns = "<th>"+componentName+"</th>";
+                    $.each(components[0]["status"], function(key, value) {
+                        var columnName = thisView.readify(key);
+                        headerColumns += "<th>" + columnName + "</th>";
+                    });
+                    $("#continuous_integration_table").append("<thead><tr>"+headerColumns+"</tr></thead>");
+                    // Build Body of Table
+                    ignored_components = []
+                    $.each(components, function(index, value) {
+                        rowContents = "<td>"+value["name"]+"</td>";
+                        $.each(value["status"], function( key, statusValue) {
+                            rowContents += "<td>"+statusValue+"</td>";
+                        });
+                        $("#continuous_integration_table").append("<tr>"+rowContents+"</tr>");
+                    });
+                }
+            } 
+        },
+
+        readify: function(string) {
+            var stringSplit = string.split("_");
+            $.each(stringSplit, function(index, value) {
+                stringSplit[index] = value.charAt(0).toUpperCase() + value.slice(1);
+            });
+            return stringSplit.join(" ");
         },
 
         openConfigManager: function(event) {
