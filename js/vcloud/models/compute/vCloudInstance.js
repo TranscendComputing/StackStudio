1ccfff19193f1ae10400992e386aff79d17b9686
phillip <pschmidt@momentumsi.com>
2014-04-14 12:15:08 -0500

    added some scaffolding for VMware integration

---
+++ js/vcloud/models/compute/vCloudInstance.js
@@ -0,0 +1,39 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+	'jquery',
+	'underscore',
+	'Backbone',
+	'common',
+	'models/resource/resourceModel'
+], function ( $, _, Backbone, common, ResourceModel ) {
+	var VCloudVmInstance = ResourceModel.extend({
+		
+		defaults : {
+			id : '',
+			name : '',
+			network_interfaces : ''
+		},
+
+		apiUrl : Common.apiUrl + "stackstudio/v1/cloud_management/vcloud/compute/instances" 
+
+		create : function ( credentialId, options ) {
+			var ajaxOptions = {
+				cred_id : credentialId,
+				"instance" : options
+			};
+			this.sendAjaxAction(this.apiUrl, "POST", ajaxOptions);
+		},
+
+		delete: function ( credentialId, region ) {
+			this.sendAjaxAction(this.apiUrl, "POST", { cred_id : credentialId }, "instanceAppRefresh");
+		} 
+	});
+
+	return VCloudVmInstance;
+});
