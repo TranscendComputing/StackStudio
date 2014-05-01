f53186ba1f2b4e7f1efb009f54864ca17a16985d
Phil <phil@phil-VirtualBox.(none)>
2014-05-01 13:20:03 -0500

    added models, collections, views for vcloud catalogs

---
+++ js/vcloud/collections/catalog/vcloudCatalogs.js
@@ -0,0 +1,24 @@
+/*!
+ * StackStudio 2.0.0-rc.1 <http://stackstudio.transcendcomputing.com>
+ * (c) 2012 Transcend Computing <http://www.transcendcomputing.com/>
+ * Available under ASL2 license <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+/*jshint smarttabs:true */
+/*global define:true console:true */
+define([
+	'backbone',
+	'common',
+	'/js/vcloud/models/catalog/vcloudCatalog.js'
+], function ( Backbone, Common, VCloudCatalog ) {
+	'use strict';
+
+	var Catalogs = Backbone.Collection.extend({
+		
+		model : VCloudCatalog,
+
+		url : Common.apiUrl + '/stackstudio/v1/cloud_management/vcloud/catalogs'
+
+	});
+
+	return Catalogs;
+});
