74d269a21506137a4b4b446f091fc7949e5706ef
Eric Thompson <ethompson@momentumsi.com>
2014-05-15 18:22:47 -0500

    Dang new git add -A!
    Forgot to add new files/dirs
    aaa_load_rjs_config.js is an attempt to have the rjs config loaded in a single place for ALL spec tests
    since all specs seem to be gathered, sorted and then executed. This still might not be sufficient when
    you wish to run a single spec file. Still developing best solution.

---
+++ spec/aaa_load_rjs_config.js
@@ -0,0 +1,1 @@
+require(['js/config/rjsConfig'], function(undefined) {});
---
+++ spec/views/accountManagementSpec.js
@@ -0,0 +1,18 @@
+/*jshint smarttabs:true */
+/*global define:true console:true describe:true it:true expect:true */
+//require(['config/rjsConfig'], function(undefined) {
+    define(
+        ['views/account/accountManagementView'],
+        function(AccountManagementView) {
+            describe("Account Management View Tests", function() {
+//                var accountManagementView = new AccountManagementView();
+    
+//                it("render is callable", function() {
+//                     renderSpy = spyOn(accountManagementView, 'render');
+//                     accountManagementView.render();
+//                     expect(accountManagementView.render).toHaveBeenCalled();
+//                });
+            });
+        }
+    );
+//});
