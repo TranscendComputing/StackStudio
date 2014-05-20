d21b6ae45acfae076fbcab27f85ff22f72b94e31
Phil <phil@phil-VirtualBox.(none)>
2014-05-20 12:46:45 -0500

    minor bugfix with new rjsConfig refactor

--- js/config/rjsConfig.js
+++ js/config/rjsConfig.js
@@ -169,6 +169,7 @@ requirejs.config({
         'instructor' : 'js/vendor/instructor',
         'mixins'     : 'js/vendor/mixins',
         'github'     : 'js/vendor/github',
-        'dataTables.bootstrap': 'js/vendor/dataTables.bootstrap'
+        'dataTables.bootstrap': 'js/vendor/dataTables.bootstrap',
+        'dataTables.fnReloadAjax': 'js/vendor/dataTables.fnReloadAjax'
     }
 });
