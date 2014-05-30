2789a4d643f28c07d4b47c48b4fb88af624ffecb
Curtis Stewart <cstewart@momentumsi.com>
2014-05-30 17:20:39 -0500

    Moved config/package.json back to root, as there are components such as travis CI that depend on that location

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('config/package.json'),
+        pkg: grunt.file.readJSON('package.json'),
         meta: {
             banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
