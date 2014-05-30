2fb4b8dbb0382a07054fa463e7b5125274eba48b
Eric Thompson <ethompson@momentumsi.com>
2014-05-30 16:02:42 -0500

    Starting better organization of spec files

--- spec/collections/stacksSpec.js
+++ spec/collections/stacksSpec.js
@@ -1,9 +1,9 @@
 /*jshint smarttabs:true */
 /*global define:true console:true describe:true it:true expect:true */
 define(['collections/stacks'], function(StackList) {
-    var stackList = new StackList();
-
     describe("StackList Collection", function() {
+        var stackList = new StackList();
+
         it("verify URL is as intended", function() {
             expect(stackList.url).toContain('/stackstudio/v1/stacks/account/');
         });
