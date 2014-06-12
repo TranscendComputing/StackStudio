d194bb055d18185d411e50423b6c2bc7ea6ca924
Eric Thompson <ethompson@momentumsi.com>
2014-06-12 17:46:35 -0500

    Think main.js is set now

--- js/main.js
+++ js/main.js
@@ -7,38 +7,30 @@
 /*global define:true console:true */
 var DEBUG = false;
 require(['config/rjsConfig'], function(undefined) {
-//    require(['common'], function (Common) {
-        if (DEBUG) {
-            requirejs.config({urlArgs: 'cb=' + Math.random()});
-        }
+    if (DEBUG) {
+        requirejs.config({urlArgs: 'cb=' + Math.random()});
+    }
 
-        require(
-            [
-                'common',
-                // Load all main navigation views here so they are accessible
-                // from top level and let all sub-views be loaded by these views
-                'views/account/navLogin',
-                'views/dashboardView',
-                //'views/projectSidebarView',
-                'views/account/accountManagementView',
-                'views/cloud_setup/cloudSetupView',
-                //'views/projectAppView',
-                //// 'views/projectNavigationSidebarView',
-                //'views/projectResourceSidebarView',
-                //// 'views/projectListItemView',
-                //'views/projectEditView',
-                'views/images/imagesView',
-                'views/platform_components/platformComponentsView',
-                'views/assemblies/assembliesView',
-                'views/stacks/stacksView',
-                'views/offerings/offeringsView',
-                'views/resource/resourceNavigationView'
-                //'views/meshes/meshesView'
-            ],
-            function(Common, NavLogin) {
-                new NavLogin().render();
-                Common.backbone.history.start();
-            }
-        );
-    //});
+    require(
+        [
+            'common',
+            // Load all main navigation views here so they are accessible
+            // from top level and let all sub-views be loaded by these views
+            'views/account/navLogin',
+            'views/dashboardView',
+            'views/account/accountManagementView',
+            'views/cloud_setup/cloudSetupView',
+            'views/images/imagesView',
+            'views/platform_components/platformComponentsView',
+            'views/assemblies/assembliesView',
+            'views/stacks/stacksView',
+            'views/offerings/offeringsView',
+            'views/resource/resourceNavigationView'
+            //'views/meshes/meshesView'
+        ],
+        function(Common, NavLogin) {
+            new NavLogin().render();
+            Common.backbone.history.start();
+        }
+    );
 });
